de6fba9df263766f08c600216ba95b9998c4af1f
GIGABYTE <max.helios@yandex.ru>
2025-03-02 13:27:05 +0300

    ДЗ-01_task_4 готово

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework_1_lesson3/task_4.java
@@ -0,0 +1,17 @@
+package ru.otus.java.basic.homeworks.homework_1_lesson3;
+
+public class task_4 {
+    public static void main(String[] args) {
+        task_4.compareNumbers();
+    }
+
+    public static void compareNumbers() {
+        int a = 10;
+        int b = 20;
+        if(a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
