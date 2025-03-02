617cddfc8332dc6c012ad4649063cda9dcfd7566
GIGABYTE <max.helios@yandex.ru>
2025-03-02 12:50:03 +0300

    ДЗ-01_task_1 готово

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework_1_lesson3/task_1.java
@@ -0,0 +1,14 @@
+package ru.otus.java.basic.homeworks.homework_1_lesson3;
+
+public class task_1 {
+    public static void main(String[] args) {
+        task_1.greetings();
+    }
+
+    private static void greetings() {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+}
